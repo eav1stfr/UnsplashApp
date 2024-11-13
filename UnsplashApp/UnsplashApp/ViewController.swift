d2ffc517e8a16d9f9f9027c510b4e632b1b780a3
eav1stfr <eavst123@gmail.com>
2024-11-13 16:04:34 +0500

    created the project

---
+++ UnsplashApp/UnsplashApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UnsplashApp
+//
+//  Created by Александр Эм on 13.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
